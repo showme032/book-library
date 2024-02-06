322b8bc45b3f4836e1dbfc054b8567d48ce0bb34
showme032 <sxomy993@gmail.com>
2024-02-06 20:30:05 +0100

    Format text files

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const books = document.querySelector('.books');
-const openModal = document.querySelector('.add-book-button');
-const modal = document.querySelector('.book-modal');
-const form = document.querySelector('.add-book-form')
-const addBook = document.querySelector('#add-book');
-const cancelBook = document.querySelector('#cancel-book');
+const books = document.querySelector(".books");
+const openModal = document.querySelector(".add-book-button");
+const modal = document.querySelector(".book-modal");
+const form = document.querySelector(".add-book-form");
+const addBook = document.querySelector("#add-book");
+const cancelBook = document.querySelector("#cancel-book");
 
 const myLibrary = [];
 
@@ -44,20 +44,20 @@ class Book {
     return this._read;
   }
 
-  info = function() {
+  info = function () {
     if (this.read === false) {
       return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
     } else {
       return `${this.title} by ${this.author}, ${this.pages} pages, already read`;
     }
-  }
+  };
 }
 
 // example books
-let hobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 310, false);
-let lotr = new Book('The Lord of the Rings', 'J. R. R. Tolkien', 1216, false);
-let silmarillion = new Book('The Silmarillion', 'J. R. R. Tolkien', 365, true);
-let crinBlanc = new Book('Crin-Blanc', 'René Guillot', 158, true);
+let hobbit = new Book("The Hobbit", "J. R. R. Tolkien", 310, false);
+let lotr = new Book("The Lord of the Rings", "J. R. R. Tolkien", 1216, false);
+let silmarillion = new Book("The Silmarillion", "J. R. R. Tolkien", 365, true);
+let crinBlanc = new Book("Crin-Blanc", "René Guillot", 158, true);
 myLibrary.push(hobbit, lotr, silmarillion, crinBlanc);
 // example books
 
@@ -65,18 +65,18 @@ myLibrary.push(hobbit, lotr, silmarillion, crinBlanc);
 displayBooks();
 
 // Open modal for adding books
-openModal.addEventListener('click', () => {
+openModal.addEventListener("click", () => {
   modal.showModal();
   form.reset();
 
   // Create object of input book in library
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     // Prevent default click action
-    e.preventDefault()
+    e.preventDefault();
 
-    let newTitle = document.getElementById('title').value;
-    let newAuthor = document.getElementById('author').value;
-    let newPages = document.getElementById('pages').value;
+    let newTitle = document.getElementById("title").value;
+    let newAuthor = document.getElementById("author").value;
+    let newPages = document.getElementById("pages").value;
     if (document.querySelector('input[name="read"]:checked').value === "true") {
       newRead = true;
     } else {
@@ -87,35 +87,35 @@ openModal.addEventListener('click', () => {
     myLibrary.push(newBook);
     displayBooks();
     modal.close();
-  })
+  });
 
   // Cancel adding book
-  cancelBook.addEventListener('click', () => {
+  cancelBook.addEventListener("click", () => {
     modal.close();
-  })
-})
+  });
+});
 
 // Delete book when requested
-document.addEventListener('click', (e) => {
-  if (e.target.matches('.del-btn')) {
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".del-btn")) {
     // console.log(e);
     removeIndex = e.target.name;
     myLibrary.splice(removeIndex, 1);
-    displayBooks()
-  } else if (e.target.matches('.rd-btn')) {
+    displayBooks();
+  } else if (e.target.matches(".rd-btn")) {
     updateBook = myLibrary[e.target.name];
     // console.log(e);
     // mby fix this up
     if (updateBook.read === true) {
       updateBook.read = false;
-      e.target.innerText = 'Not read'
+      e.target.innerText = "Not read";
     } else {
       updateBook.read = true;
-      e.target.innerText = 'Read'
+      e.target.innerText = "Read";
     }
     displayBooks();
   }
-})
+});
 
 // Display all the books from the library
 function displayBooks() {
@@ -129,27 +129,26 @@ function displayBooks() {
     let author = book.author;
     let pages = book.pages;
     if (book.read === true) {
-      read = 'Read'
+      read = "Read";
     } else {
-      read = 'Not read'
+      read = "Not read";
     }
-  
-  
+
     // And add it to the table on the page
-    let bookrow = document.createElement('tr');
-    let readClass = ''
-    if (read === 'Read') {
-      readClass = 'read';
+    let bookrow = document.createElement("tr");
+    let readClass = "";
+    if (read === "Read") {
+      readClass = "read";
     } else {
-      readClass = 'not-read';
+      readClass = "not-read";
     }
     // bookrow.className = `book-${index}`
     bookrow.innerHTML = `<td>${title}</td>
                          <td>${author}</td>
                          <td>${pages}</td>
                          <td><button name=${index} class="rd-btn ${readClass}">${read}</button></td>
-                         <td><button name=${index} class="del-btn">Delete</button></td>`
-                         
+                         <td><button name=${index} class="del-btn">Delete</button></td>`;
+
     books.appendChild(bookrow);
 
     // Delete book when requested
